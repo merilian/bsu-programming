d6c57780492219bec67c93476c9228f5cdf4ead0
Evgeniy Derevyago <merilian@yandex.ru>
2024-03-22 07:47:46 +0300

    python

---
+++ python/modules.py
@@ -0,0 +1,18 @@
+#import math
+#print(math.pi)
+
+#import math as m
+#print(m.pi)
+
+#from math import pi, cos
+#print(pi)
+
+#import start
+#print(start.square(2))
+
+#import class_test
+from class_test import Point3D, p3
+
+p4 = Point3D(3, 3, 3)
+print(p4.dist())
+print(p3.dist())
